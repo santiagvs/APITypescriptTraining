7a91b8df7ce979807e6192d5329c2e5bd481c7b3
Santiago <victor.lacsilva0102@gmail.com>
2022-09-29 12:06:19 -0300

    feat: adding typeorm and sqlite to create table and run migrations

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,8 +1,9 @@
 import { DataSource } from 'typeorm'
+import { CreateRolesTable1664463205974 } from './migrations/1664463205974-CreateRolesTable'
 
 export const dataSource = new DataSource({
   type: 'sqlite',
   database: './db.sqlite',
   entities: [],
-  migrations: [],
+  migrations: [CreateRolesTable1664463205974],
 })
