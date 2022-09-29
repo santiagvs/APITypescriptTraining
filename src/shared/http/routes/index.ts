828e6ed16d77bd58db934b6d5672287c7c7c6275
Santiago <victor.lacsilva0102@gmail.com>
2022-09-28 21:28:18 -0300

    feat: initialized first route

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,11 +1,12 @@
-import { AppError } from '@shared/errors/AppError'
 import { Router } from 'express'
+import { rolesRouter } from '@roles/http/routes/roles.routes'
 
 const routes = Router()
 
 routes.get('/', (req, res) => {
-  throw new AppError('Acesso negado')
   return res.json({ message: 'Olá dev!' })
 })
+
+routes.use('/roles', rolesRouter)
 
 export { routes }
