720a38d64676845f950589c58d542c428d944745
Santiago <victor.lacsilva0102@gmail.com>
2022-09-28 23:31:21 -0300

    fix: configuration to set RoleRepository to singleton

--- src/roles/useCases/createRole/index.ts
+++ src/roles/useCases/createRole/index.ts
@@ -2,6 +2,6 @@ import { RolesRepository } from '@roles/repositories/RolesRepository'
 import { CreateRoleController } from './createRoleController'
 import { CreateRoleUseCase } from './createRoleUseCase'
 
-const rolesRepository = new RolesRepository()
+const rolesRepository = RolesRepository.getInstance()
 const createRoleUseCase = new CreateRoleUseCase(rolesRepository)
 export const createRolesController = new CreateRoleController(createRoleUseCase)
