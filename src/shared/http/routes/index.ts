852dd8f29dd62cf83596b5fa21027400594095ef
Santiago <victor.lacsilva0102@gmail.com>
2022-09-28 21:13:07 -0300

    feat: adding middleware for Errors

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,8 +1,10 @@
+import { AppError } from '@shared/errors/AppError'
 import { Router } from 'express'
 
 const routes = Router()
 
 routes.get('/', (req, res) => {
+  throw new AppError('Acesso negado')
   return res.json({ message: 'Olá dev!' })
 })
 
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config'
-import express from 'express'
+import { AppError } from '@shared/errors/AppError'
+import express, { NextFunction, Request, Response } from 'express'
 import 'express-async-errors'
 import cors from 'cors'
 import { routes } from './routes'
@@ -9,6 +10,22 @@ app.use(cors())
 app.use(express.json())
 
 app.use(routes)
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: error,
+        message: error.message,
+      })
+    }
+    console.log(error)
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    })
+  },
+)
 
 app.listen(process.env.PORT, () => {
   console.log(`listening on port ${process.env.PORT}! 🍻`)
