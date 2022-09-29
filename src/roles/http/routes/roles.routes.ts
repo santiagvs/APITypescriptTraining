33d12784e494761943d4d9105724f6c2c224d71e
Santiago <victor.lacsilva0102@gmail.com>
2022-09-28 22:05:37 -0300

    feat: adding Role Repository

--- src/roles/http/routes/roles.routes.ts
+++ src/roles/http/routes/roles.routes.ts
@@ -1,21 +1,12 @@
-import { Role } from '@roles/entities/Role'
+import { RolesRepository } from '@roles/repositories/RolesRepository'
 import { Router } from 'express'
 
 const rolesRouter = Router()
-
-const roles: Role[] = []
+const rolesRepository = new RolesRepository()
 
 rolesRouter.post('/', (req, res) => {
   const { name } = req.body
-
-  const role = new Role()
-
-  Object.assign(role, {
-    name,
-    created_at: new Date(),
-  })
-
-  roles.push(role)
+  const role = rolesRepository.create({ name })
 
   return res.status(201).json(role)
 })
