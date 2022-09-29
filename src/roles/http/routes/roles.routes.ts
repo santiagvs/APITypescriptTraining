28737b7ec49108b414c64bd2f06f062bc2d30102
Santiago <victor.lacsilva0102@gmail.com>
2022-09-28 22:15:12 -0300

    feat: created method to find by name on roles

--- src/roles/http/routes/roles.routes.ts
+++ src/roles/http/routes/roles.routes.ts
@@ -6,6 +6,14 @@ const rolesRepository = new RolesRepository()
 
 rolesRouter.post('/', (req, res) => {
   const { name } = req.body
+  const roleAlreadyExists = rolesRepository.findByName(name)
+
+  if (roleAlreadyExists) {
+    return res.status(400).json({
+      error: 'A role with this name already exists.',
+    })
+  }
+
   const role = rolesRepository.create({ name })
 
   return res.status(201).json(role)
