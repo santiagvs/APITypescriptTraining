1738fd26068340c228665a0b5ecc3fdd6c359108
Santiago <victor.lacsilva0102@gmail.com>
2022-09-30 14:08:40 -0300

    feat: adding update role usecase

---
+++ src/roles/useCases/updateRole/updateRoleController.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express'
+import { UpdateRoleUseCase } from './updateRoleUseCase'
+
+export class UpdateRoleController {
+  constructor(private updateRoleUseCase: UpdateRoleUseCase) {}
+
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params
+    const { name } = request.body
+    const role = await this.updateRoleUseCase.execute({ id, name })
+
+    return response.json(role)
+  }
+}
