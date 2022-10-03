9a28d72c9b30703605fe67d4a4610ba0aeb327f5
Santiago <victor.lacsilva0102@gmail.com>
2022-10-03 20:33:43 -0300

    feat: adding Celebrate/Joi and validation to routes

--- src/shared/http/app.ts
+++ src/shared/http/app.ts
@@ -3,6 +3,7 @@ import express, { NextFunction, Request, Response } from 'express'
 import 'express-async-errors'
 import swaggerUi from 'swagger-ui-express'
 import cors from 'cors'
+import { errors } from 'celebrate'
 import { routes } from './routes'
 import swaggerFile from '../../swagger.json'
 
@@ -10,8 +11,9 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
-
 app.use(routes)
+app.use(errors())
+
 app.use(
   (error: Error, request: Request, response: Response, next: NextFunction) => {
     if (error instanceof AppError) {
@@ -21,7 +23,6 @@ app.use(
       })
     }
     console.log(error)
-
     return response.status(500).json({
       status: 'error',
       message: 'Internal server error',
