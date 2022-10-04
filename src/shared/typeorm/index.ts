883def4ef7d0fc21fedf2781616496c056697d24
Santiago <victor.lacsilva0102@gmail.com>
2022-10-04 14:46:30 -0300

    feat: created User entity

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,4 +1,5 @@
 import { Role } from '@roles/entities/Role'
+import { User } from '@users/entities/User'
 import { DataSource } from 'typeorm'
 import { CreateRolesTable1664463205974 } from './migrations/1664463205974-CreateRolesTable'
 import { CreateUsersTable1664903968892 } from './migrations/1664903968892-CreateUsersTable'
@@ -7,7 +8,7 @@ import { AddRoleIdToUsersTable1664904989396 } from './migrations/1664904989396-A
 export const dataSource = new DataSource({
   type: 'sqlite',
   database: './db.sqlite',
-  entities: [Role],
+  entities: [Role, User],
   migrations: [
     CreateRolesTable1664463205974,
     CreateUsersTable1664903968892,
