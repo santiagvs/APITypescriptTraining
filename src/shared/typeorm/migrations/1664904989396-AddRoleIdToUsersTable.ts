17adf56bc41dc02406d2be6d3c3bd0c48210012b
Santiago <victor.lacsilva0102@gmail.com>
2022-10-04 14:41:49 -0300

    feat: created migration for add roleId to Users table

---
+++ src/shared/typeorm/migrations/1664904989396-AddRoleIdToUsersTable.ts
@@ -0,0 +1,35 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  TableColumn,
+  TableForeignKey,
+} from 'typeorm'
+
+export class AddRoleIdToUsersTable1664904989396 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'users',
+      new TableColumn({
+        name: 'roleId',
+        type: 'uuid',
+        isNullable: true,
+      }),
+    )
+
+    await queryRunner.createForeignKey(
+      'users',
+      new TableForeignKey({
+        name: 'UsersRoles',
+        columnNames: ['roleId'],
+        referencedTableName: 'roles',
+        referencedColumnNames: ['id'],
+        onDelete: 'SET NULL',
+      }),
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('users', 'UsersRoles')
+    await queryRunner.dropColumn('users', 'roleId')
+  }
+}
