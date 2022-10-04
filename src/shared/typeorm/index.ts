b9f8ec854ef999a30a6d73aea348a9846c5dedc6
Santiago <victor.lacsilva0102@gmail.com>
2022-10-04 14:35:29 -0300

    feat: created migration for Users table

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,10 +1,11 @@
 import { Role } from '@roles/entities/Role'
 import { DataSource } from 'typeorm'
 import { CreateRolesTable1664463205974 } from './migrations/1664463205974-CreateRolesTable'
+import { CreateUsersTable1664903968892 } from './migrations/1664903968892-CreateUsersTable'
 
 export const dataSource = new DataSource({
   type: 'sqlite',
   database: './db.sqlite',
   entities: [Role],
-  migrations: [CreateRolesTable1664463205974],
+  migrations: [CreateRolesTable1664463205974, CreateUsersTable1664903968892],
 })
