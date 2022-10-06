79a36bd586147dfe08a372c29ab7c2b67d221aca
Santiago <victor.lacsilva0102@gmail.com>
2022-10-06 14:14:11 -0300

    feat: added listuser routes and controller

---
+++ src/users/useCases/listUsers/ListUsersUseCase.ts
@@ -0,0 +1,28 @@
+import { inject, injectable } from 'tsyringe'
+import {
+  IUsersRepository,
+  UsersPaginateProperties,
+} from '@users/repositories/IUsersRepository'
+
+type ListUsersUseCaseParams = {
+  page: number
+  limit: number
+}
+
+@injectable()
+export class ListUsersUseCase {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+  ) {}
+
+  async execute({
+    page,
+    limit,
+  }: ListUsersUseCaseParams): Promise<UsersPaginateProperties> {
+    const take = limit
+    const skip = (Number(page) - 1) * take
+
+    return this.usersRepository.findAll({ page, skip, take })
+  }
+}
