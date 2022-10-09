4b2ed446b643dd85ad0eb815b66a5b2fe9b174c4
Santiago <victor.lacsilva0102@gmail.com>
2022-10-09 14:28:26 -0300

    feat: adding routes and controller for Update avatar

--- src/users/http/users.routes.ts
+++ src/users/http/users.routes.ts
@@ -5,11 +5,16 @@ import { CreateUserController } from '@users/useCases/createUser/CreateUserContr
 import { ListUsersController } from '@users/useCases/listUsers/ListUsersController'
 import { CreateLoginController } from '@users/useCases/createLogin/CreateLoginController'
 import { isAuthenticated } from '@shared/http/middlewares/isAuthenticated'
+import { UpdateAvatarController } from '@users/useCases/updateAvatar/UpdateAvatarController'
+import multer from 'multer'
+import uploadConfig from '@config/upload'
 
 const usersRouter = Router()
 const createUserController = container.resolve(CreateUserController)
 const listUsersController = container.resolve(ListUsersController)
 const createLoginController = container.resolve(CreateLoginController)
+const updateAvatarController = container.resolve(UpdateAvatarController)
+const upload = multer(uploadConfig)
 
 usersRouter.post(
   '/',
@@ -52,6 +57,15 @@ usersRouter.post(
   }),
   (request, response) => {
     return createLoginController.handle(request, response)
+  },
+)
+
+usersRouter.patch(
+  '/avatar',
+  isAuthenticated,
+  upload.single('avatar'),
+  (request, response) => {
+    return updateAvatarController.handle(request, response)
   },
 )
 
